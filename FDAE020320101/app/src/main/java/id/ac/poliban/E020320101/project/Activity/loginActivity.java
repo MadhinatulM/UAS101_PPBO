6f261abec46f38a9c8d34faf4c3d85cadf8e362b
MadhinatulM <madhinatulm@gmail.com>
2023-01-15 21:46:46 +0700

    E020320101_VD MI

---
+++ FDAE020320101/app/src/main/java/id/ac/poliban/E020320101/project/Activity/loginActivity.java
@@ -0,0 +1,6 @@
+package id.ac.poliban.E020320101.project.Activity;
+
+import id.ac.poliban.E020320101.project.R;
+
+public class loginActivity {
+}
---
+++ FDAE020320101/app/src/main/java/id/ac/poliban/E020320101/project/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.E020320101.project.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
